08761efdb1fb8fe150aad32f7fea865a97beb458
markusrc11 <markusrc11@hotmail.com>
2017-04-17 00:44:56 +0200

    Update and rename .gitignore to main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Metode {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		comptadorDeParells(n);
+		sc.close();
+	}
+
+	static void comptadorDeParells(int n) {
+		int count = 0;
+		for (int countParell = n; countParell >= 0; countParell--) {
+			if (countParell % 2 == 0)
+				count++;
+		}
+		System.out.println(count);
+	}
+
+}
